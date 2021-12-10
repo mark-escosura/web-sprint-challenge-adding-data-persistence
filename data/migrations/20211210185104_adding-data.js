d83b35f172f29d5ae62e5ac211e15b13e1fe3376
mark-escosura <escosuramarkse@gmail.com>
2021-12-10 11:06:06 -0800

    Completed resource and task table inside my migration file

--- data/migrations/20211210185104_adding-data.js
+++ data/migrations/20211210185104_adding-data.js
@@ -1,18 +1,36 @@
-
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema
-  .createTable("project", (table) => {
-    table.increments("project_id");
-    table.string("project_name", 128).notNullable();
-    table.string("project_description")
-    table.boolean('project_completed').defaultTo(false);
-  })
+    .createTable("project", (table) => {
+      table.increments("project_id");
+      table.string("project_name", 128).notNullable();
+      table.string("project_description");
+      table.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("resource", (table) => {
+      table.increments("resource_id");
+      table.string("resource_name", 128).notNullable().unique();
+      table.string("resource_description");
+    })
+    .createTable("task", (table) => {
+      table.increments("task_id");
+      table.string("task_description").notNullable();
+      table.string("task_notes");
+      table.boolean("task_completed").defaultTo(false);
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("project")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT"); 
+    });
 };
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
   await knex.schema
-  .dropTableIfExist()
-  .dropTableIfExist()
-  .dropTableIfExist()
-  .dropTableIfExist()
+    .dropTableIfExist()
+    .dropTableIfExist()
+    .dropTableIfExist()
+    .dropTableIfExist();
 };
