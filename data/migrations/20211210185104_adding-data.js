c7d15bbd368ef1b763a2194d7c5f382587342328
mark-escosura <escosuramarkse@gmail.com>
2021-12-10 10:59:17 -0800

    Created my project table

---
+++ data/migrations/20211210185104_adding-data.js
@@ -0,0 +1,18 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+  .createTable("project", (table) => {
+    table.increments("project_id");
+    table.string("project_name", 128).notNullable();
+    table.string("project_description")
+    table.boolean('project_completed').defaultTo(false);
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+  .dropTableIfExist()
+  .dropTableIfExist()
+  .dropTableIfExist()
+  .dropTableIfExist()
+};
