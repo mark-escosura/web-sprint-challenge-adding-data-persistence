3e085e59e80fe207801caa6c3321cb96dfb3e491
mark-escosura <escosuramarkse@gmail.com>
2021-12-10 10:45:56 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
 // build your server here and require it from index.js
+const express = require("express") // require express from express package
+
+const server = express() // declare the express function into a variable called server
+
+server.use(express.json()) // express function is a built-in middleware function in Express
+
+server.use((err,req,res,next) => {
+    next({
+        status: err.status || 500,
+        message: err.message,
+        stack: err.stack
+    })
+}) // errorHandling
+
+module.exports = server
